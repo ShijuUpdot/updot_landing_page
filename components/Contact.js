32b4af135c91773b7bf9fed8d6b4bab91a6c2d41
Srijan Gupta <srijan@updot.in>
2022-11-02 13:45:13 +0530

    feat: contact us

--- components/Contact.js
+++ components/Contact.js
@@ -1,143 +1,297 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [companyName, setCompanyName] = useState('')
+  const [companyWebsite, setCompanyWebsite] = useState('')
+  const [message, setMessage] = useState('')
+  const [status, setStatus] = useState('Submit')
+
   return (
-    <div className="container mx-auto px-4 flex relative">
-      <form
-        action="#"
-        method="GET"
-        className="w-full max-w-full py-4 sm:px-24 xl:px-40  mx-auto"
-      >
-        <div className="mb-5">
-          <label
-            htmlFor="name"
-            className="mb-3 block text-xl font-medium text-[#1C1C1C]"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Your name"
-            min="0"
-            className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+    <div className="container mx-auto px-4 relative flex flex-col md:flex-row  justify-center items-center gap-10">
+      {/* Left Side */}
+      <div className="w-full md:w-1/2 flex flex-col self-start relative order-1 md:order-first">
+        <div className="py-9 px-2">
+          <Image
+            src="/logo-alt.png"
+            alt="Logo"
+            width={200}
+            height={200}
+            className="mb-4"
           />
         </div>
-        <div className="-mx-3 flex flex-wrap">
-          <div className="w-full px-3 sm:w-1/2">
-            <div className="mb-5">
-              <label
-                htmlFor="companyName"
-                className="mb-3 block text-xl font-medium text-[#1C1C1C]"
-              >
-                Company Name
-              </label>
-              <input
-                type="text"
-                name="companyName"
-                id="companyName"
-                placeholder="Company name"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-              />
+        <div className="flex flex-col gap-4">
+          <div className="px-2 py-3">
+            <h3 className="text-2xl font-semibold mb-4 text-[#090914]">
+              Our Address
+            </h3>
+            <div className="flex items-center justify-start">
+              <div className="mr-1 flex items-center">
+                <Image
+                  src="/icons/location.svg"
+                  alt="Location"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <p className="ml-2 text-[#090914] text-xl">
+                No.32, next to spring valley club, Bengaluru,
+                <br />
+                Karnataka 560090
+              </p>
             </div>
           </div>
-          <div className="w-full px-3 sm:w-1/2">
-            <div className="mb-5">
-              <label
-                htmlFor="Company Website"
-                className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+          <hr className="border-[#95959F] mb-4 max-w-xl" />
+          <div className="px-2 py-3">
+            <h3 className="text-2xl font-semibold mb-4 text-[#090914]">
+              Call Us
+            </h3>
+            <div className="flex items-center justify-start">
+              <div className="mr-1 flex items-center">
+                <Image
+                  src="/icons/telephone.svg"
+                  alt="Location"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <Link
+                href="tel:+917353311555"
+                className="ml-2 text-[#090914] text-xl"
               >
-                Company Website
-              </label>
-              <input
-                type="text"
-                name="Company Website"
-                id="Company Website"
-                placeholder="Company Website"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-              />
+                +91 7353311555
+              </Link>
             </div>
           </div>
+          <hr className="border-[#95959F] mb-4 max-w-xl" />
+          <div className="px-2 py-3">
+            <h3 className="text-2xl font-semibold mb-4 text-[#090914]">
+              Our Address
+            </h3>
+            <div className="flex items-center justify-start">
+              <div className="mr-1 flex items-center">
+                <Image
+                  src="/icons/envelope.svg"
+                  alt="Mail"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <Link
+                href="mailto:hello@updot.in"
+                className="ml-2 text-[#090914] text-xl"
+              >
+                hello@updot.in
+              </Link>
+            </div>
+          </div>
+          <hr className="border-[#95959F] mb-4 max-w-xl" />
         </div>
+      </div>
+      {/* Right Side */}
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-start relative">
+        <form action="#" method="GET" className="w-full pl-0 md:pl-4">
+          <div className="mb-5">
+            <label
+              htmlFor="name"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Full name"
+              min="0"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email address"
+              min="0"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="companyName"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Company Name
+            </label>
+            <input
+              type="text"
+              name="companyName"
+              id="companyName"
+              placeholder="Company name"
+              min="0"
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+              className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="companyWebsite"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Company Website
+            </label>
+            <input
+              type="text"
+              name="companyWebsite"
+              id="companyWebsite"
+              placeholder="Company website"
+              min="0"
+              value={companyWebsite}
+              onChange={(e) => setCompanyWebsite(e.target.value)}
+              className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="companyWebsite"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Choose the services that best fits your need.
+            </label>
+            <div className="grid grid-cols-2 grid-rows-3 gap-4">
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  UI/UX Design
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  Marketing & Content
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  Web Development
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  User Research Workshop
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  Brand Strategy
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="service1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 accent-gray-900 rounded border-gray-300"
+                />
+                <label
+                  htmlFor="service1"
+                  class="ml-2 w-full text-xl font-normal text-[#1C1C1C]"
+                >
+                  Other
+                </label>
+              </div>
+            </div>
+          </div>
 
-        <div className="-mx-3 flex flex-wrap">
-          <div className="w-full flex flex-wrap px-3 sm:w-1/2">
-            <div className="mb-5 md:pr-3 sm:w-1/4">
-              <label
-                htmlFor="phoneNumber"
-                className="mb-3 block text-xl font-medium text-[#1C1C1C]"
-              >
-                Phone&nbsp;Number
-              </label>
-              <select
-                type="text"
-                name="countryCode"
-                id="countryCode"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-2 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-              >
-                <option>IND(+91)</option>
-                <option>EUR(+43)</option>
-                <option>US(+1)</option>
-              </select>
-            </div>
-            <div className="mb-5 md:pl-3 sm:w-3/4">
-              <label
-                htmlFor="countryCode"
-                className="mb-3 text-xl font-medium text-[#1C1C1C] hidden md:block"
-              >
-                &nbsp;
-              </label>
-              <input
-                type="text"
-                name="phoneNumber"
-                id="phoneNumber"
-                placeholder="Phone number"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-              />
-            </div>
+          <div className="mb-5">
+            <label
+              htmlFor="details"
+              className="mb-3 block text-xl font-medium text-[#1C1C1C]"
+            >
+              Message
+            </label>
+            <textarea
+              rows={5}
+              name="details"
+              id="details"
+              placeholder="Type your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
+            />
           </div>
-          <div className="w-full px-3 sm:w-1/2">
-            <div className="mb-5">
-              <label
-                htmlFor="email"
-                className="mb-3 block text-xl font-medium text-[#1C1C1C]"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-              />
-            </div>
+
+          <div className="px-2 py-3">
+            <button className="hover:shadow-form rounded-xl bg-gray-900 py-3 px-16 text-center text-xl font-semibold text-white outline-none">
+              {status}
+            </button>
           </div>
-        </div>
-
-        <div className="mb-5">
-          <label
-            htmlFor="name"
-            className="mb-3 block text-xl font-medium text-[#1C1C1C]"
-          >
-            Project Details (optional)
-          </label>
-          <textarea
-            rows={5}
-            name="details"
-            id="details"
-            placeholder="Please discribe the project in brief"
-            className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-xl font-medium text-[#6B7280] outline-none focus:border-[#1C1C1C] focus:shadow-md"
-          />
-        </div>
-
-        <div className="flex justify-center">
-          <button className="hover:shadow-form rounded-md bg-gray-900 py-3 px-16 text-center text-xl font-semibold text-white outline-none">
-            Submit
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
