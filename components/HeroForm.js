55ef2784333ed931ab22dab6a6f8047bd264ff63
Srijan Gupta <srijan@updot.in>
2022-11-08 12:11:30 +0530

    fix: form validation

--- components/HeroForm.js
+++ components/HeroForm.js
@@ -2,14 +2,28 @@ import React, { useState } from 'react'
 import IconButton from './IconButton'
 import { useForm, ValidationError } from '@formspree/react'
 import Thanks from './Thanks'
+import { Formik, Field, Form } from 'formik'
+import * as Yup from 'yup'
+import FormValError from './FormValError'
 
 const HeroForm = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [companyName, setCompanyName] = useState('')
-  const [mobileNumber, setMobileNumber] = useState('')
-  const [message, setMessage] = useState('')
   const [state, handleSubmit] = useForm('xgeqrepg')
+
+  const heroFormSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Name cannot be empty'),
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Email cannot be empty'),
+    companyName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Company Name cannot be empty'),
+    mobileNumber: Yup.string().required('Mobile Number cannot be empty'),
+    message: Yup.string().min(2, 'Too Short!').max(500, 'Too Long!'),
+  })
 
   if (state.succeeded) {
     return <Thanks />
@@ -20,121 +34,150 @@ const HeroForm = () => {
       <h3 className="uppercase lg:text-xl xl:text-2xl font-bold text-gray-900 text-center w-full pb-4">
         Get in Touch
       </h3>
-      <form onSubmit={handleSubmit} className="w-full">
-        <div className="mb-5">
-          <label
-            htmlFor="name"
-            className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
-          >
-            Full Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            min="0"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
-          />
-          <ValidationError prefix="Name" field="name" errors={state.errors} />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
-          >
-            Email Address
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            min="0"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="companyName"
-            className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
-          >
-            Company Name
-          </label>
-          <input
-            type="text"
-            name="companyName"
-            id="companyName"
-            placeholder="Company name"
-            min="0"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-            className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
-          />
-          <ValidationError
-            prefix="Company Name"
-            field="companyName"
-            errors={state.errors}
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="mobileNumber"
-            className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
-          >
-            Mobile Number
-          </label>
-          <input
-            type="text"
-            name="mobileNumber"
-            id="mobileNumber"
-            placeholder="Mobile Number"
-            min="0"
-            value={mobileNumber}
-            onChange={(e) => setMobileNumber(e.target.value)}
-            className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
-          />
-          <ValidationError
-            prefix="Mobile Number"
-            field="mobileNumber"
-            errors={state.errors}
-          />
-        </div>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          companyName: '',
+          mobileNumber: '',
+          message: '',
+        }}
+        validationSchema={heroFormSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit(values)
+          setSubmitting(false)
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Form onSubmit={handleSubmit} className="w-full">
+            <div className="mb-5">
+              <label
+                htmlFor="name"
+                className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
+              >
+                Full Name
+              </label>
+              <Field
+                type="text"
+                name="name"
+                placeholder="Name"
+                min="0"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
+              />
+              <FormValError>
+                {errors.name && touched.name && errors.name}
+              </FormValError>
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="email"
+                className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
+              >
+                Email Address
+              </label>
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email"
+                min="0"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
+              />
+              <FormValError>
+                {errors.email && touched.email && errors.email}
+              </FormValError>
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="companyName"
+                className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
+              >
+                Company Name
+              </label>
+              <Field
+                type="text"
+                name="companyName"
+                placeholder="Company name"
+                min="0"
+                value={values.companyName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
+              />
+              <FormValError>
+                {errors.companyName &&
+                  touched.companyName &&
+                  errors.companyName}
+              </FormValError>
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="mobileNumber"
+                className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
+              >
+                Mobile Number
+              </label>
+              <Field
+                type="text"
+                name="mobileNumber"
+                placeholder="Mobile Number"
+                min="0"
+                value={values.mobileNumber}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
+              />
+              <FormValError>
+                {errors.mobileNumber &&
+                  touched.mobileNumber &&
+                  errors.mobileNumber}
+              </FormValError>
+            </div>
 
-        <div className="mb-5">
-          <label
-            htmlFor="details"
-            className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
-          >
-            Message
-          </label>
-          <textarea
-            rows={3}
-            name="details"
-            id="details"
-            placeholder="Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
-          />
-          <ValidationError
-            prefix="Message"
-            field="details"
-            errors={state.errors}
-          />
-        </div>
+            <div className="mb-5">
+              <label
+                htmlFor="message"
+                className="mb-3 text-base md:text-xl hidden font-medium text-[#1C1C1C]"
+              >
+                Message
+              </label>
+              <Field
+                as="textarea"
+                rows={3}
+                name="message"
+                placeholder="Message"
+                value={values.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="w-full appearance-none rounded-xl border border-[#1C1C1C] bg-white py-2 px-2 xl:py-3 xl:px-3 text-base font-medium text-[#6D6969] outline-none focus:border-[#1C1C1C] focus:shadow-md placeholder-[#6D6969]"
+              />
+              <FormValError>
+                {errors.message && touched.message && errors.message}
+              </FormValError>
+            </div>
 
-        <div className="px-2 py-1 w-fit mx-auto">
-          <IconButton icon="/icons/arrow_right.svg" order={2} type="submit">
-            Send Message
-          </IconButton>
-        </div>
-      </form>
+            <div className="px-2 py-1 w-fit mx-auto">
+              <IconButton icon="/icons/arrow_right.svg" order={2} type="submit">
+                Send Message
+              </IconButton>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
