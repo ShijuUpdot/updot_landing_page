28f54da2a52ef704748b1aad51b64b2eb0d9ad2d
sourab <sourab@updot.in>
2022-11-18 15:03:56 +0530

    fixes

--- components/floating_button.js
+++ components/floating_button.js
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types'
 const FloatingButton = ({ link, children }) => {
   return (
     <div className="fixed bottom-12 right-12 w-16 h-16 z-50 bg-white rounded-full flex items-center justify-center shadow-2xl shadow-slate-800 cursor-pointer">
-      <Link href={link}>{children}</Link>
+      <Link href={link} target="_blank">
+        {children}
+      </Link>
     </div>
   )
 }
